90f22d1b62b93d98381f80b1c8ec991948901d0b
paula_ <75187606+polly-em@users.noreply.github.com>
2021-03-30 16:53:17 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const overlay = document.querySelector('#overlay');
-const phrase = document.querySelector('#phrase');
 const startButton = document.getElementsByClassName('btn__reset')[0];
+const phrase = document.querySelector('#phrase');
 const keyboard = document.querySelector('#qwerty');
-const startScreen = document.querySelector('.start');
-const letters = document.querySelectorAll('.letter');
-const misses = document.querySelector('.misses');
+const button = document.getElementsByTagName('button');
+
 
 // attaches an event listener to the “Start Game” button to hide the start screen overlay
-startButton.addEventListener('click', hideStartScreen);
-keyboard.addEventListener('click', keyboardPress);
+startButton.addEventListener('click', () => {
+  overlay.style.display = "none";
+});
 
 // number of guesses player has missed is set to 0
 let missed = 0;
@@ -25,12 +25,43 @@ const phrases = [
 
 // returns a random phrase from an array
 
+const getRandomPhraseAsArray = (arr) => {
+  let i = Math.floor(Math.random() * arr.length);
+  return arr[i].split('');
+};
+
+getRandomPhraseAsArray(phrases);
 
 // adds the letters of a string to the display
+function addPhraseToDisplay(arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    let li = document.createElement('li');
+    li.textContent = arr[i].toLowerCase();
+    phrase.appendChild(li);
+    if (li.textContent === ' ') {
+      li.className = 'space';
+    } else {
+      li.className = 'letter';
 
+    }
+  }
+}
+addPhraseToDisplay(randomPhrase);
 
 // check if a letter is in the phrase
-
+function checkLetter(button) {
+  const letter = document.querySelectorAll('li');
+  let match = null;
+    for (let i = 0; i < letter.length; i++) {
+      if (button === letter[i].textContent.toLowerCase()) {
+        letter[i].classList.add('show');
+        match = true;
+      }
+      
+    };
+  
+    return match;
+  };
 // check if the game has been won or lost
 
 
@@ -40,5 +71,6 @@ const phrases = [
 // resets the game
 
 // listen for the start game button to be pressed
+
 
 // keyboard event listener
